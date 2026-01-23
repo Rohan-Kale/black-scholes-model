26e1dd76d906c1f754700c42f52a21d3a81dfab9
Rohan Kale <kalerohan42@gmail.com>
2026-01-22 20:39:00 -0500

    Modularized code

---
+++ src/analytics/greek_variables.py
@@ -0,0 +1,59 @@
+import yfinance as yf
+import pandas as pd
+import datetime as dt
+
+def get_stock_price(ticker):
+    S = ticker.history(period="1mo")["Close"].iloc[-1]
+    return S
+
+
+def get_risk_free_rate():
+    r = yf.Ticker("^IRX").history(period="1mo")["Close"].iloc[-1] / 100
+    return r
+
+def get_call_data(ticker):
+    expiration_dates = ticker.options
+    first_expiry = expiration_dates[0]
+    ticker_chain = ticker.option_chain(first_expiry)
+
+    calls = ticker_chain.calls
+    return calls
+
+def get_strike_price(calls):
+    K = float(input("What strike price would you like to us: "))
+    idx = (calls["strike"] - K).abs().argmin()              #grab idx of closest strike to K
+    K = calls.iloc[idx]["strike"] 
+    
+    return K
+
+def get_volatility(calls, strike_price):
+    idx = (calls["strike"] - strike_price).abs().argmin()   
+    sigma = calls.iloc[idx]["impliedVolatility"] / 100
+    
+    return sigma
+
+def get_dividend_yield(ticker):
+    q = ticker.info.get("dividendYield")/100 or 0.0          #ensure some dividend specified (0 if no dividend for that option)
+    if q < 0.01:
+        q = 0.0
+    
+    return q
+
+def find_expiry(ticker):
+    now = dt.datetime.now()
+    
+    # find the next valid expiry date
+    first_expiry = None
+    for expiry in ticker.options:
+        expiry_dt = dt.datetime.strptime(expiry, "%Y-%m-%d").replace(hour=16)
+        if expiry_dt > now + dt.timedelta(days=30):
+            first_expiry = expiry
+            break
+    
+    if first_expiry is None:
+        raise ValueError("No valid future expirations")        
+    
+    expiry_dt = dt.datetime.strptime(first_expiry, "%Y-%m-%d").replace(hour=16)     
+    T = (expiry_dt - now).total_seconds() / (365.25 * 24 * 3600)
+    
+    return T
--- src/model/black_scholes.py
+++ src/model/black_scholes.py
@@ -3,63 +3,44 @@ import pandas as pd
 import scipy.stats as stats
 import numpy as np
 import datetime as dt
+import sys
+import os
 
+# gain access to greek variables
+src_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, src_dir)
+
+from analytics import greek_variables as gv
 
 def generate_greeks(ticker):
     # Get Ticker object and expires
     ticker = yf.Ticker(ticker)
 
-    q = ticker.info.get("dividendYield")/100 or 0.0 # ensure some dividend specified (0 if no dividend for that option)
+    # Get dividend yield
+    q = gv.get_dividend_yield(ticker)
 
     # Current stock price
-    S = ticker.history(period="5d")["Close"].iloc[-1]
+    S = gv.get_stock_price(ticker)
     
     # 13-week T-bill ( risk-free rate )
-    r = yf.Ticker("^IRX").history(period="5d")["Close"].iloc[-1] / 100
+    r = gv.get_risk_free_rate()
 
-    expiration_dates = ticker.options
-    first_expiry = expiration_dates[0]
-    ticker_chain = ticker.option_chain(first_expiry)
-
-    calls = ticker_chain.calls
+    # get closest matching strike price to user inp
+    calls = gv.get_call_data(ticker)
+    K = gv.get_strike_price(calls)
     
-    ''' Using a specific strike price and the atm IV for that strike price'''
-    
-    ### atm_call = calls.iloc[(calls["strike"]-S).abs().argmin()]       
-    ### sigma = atm_call["impliedVolatility"]       # ATM volatility
-    
-    # K = atm_call["strike"]            !! atm strike price !!
-    
-    ''' User defined strike price and finding IV for that value'''
-    K = float(input("What strike price would you like to us: "))
-    
-    idx = (calls["strike"] - K).abs().argmin()       # grab idx of closest strike to K
-    K = calls.iloc[idx]["strike"] 
-    sigma = calls.iloc[idx]["impliedVolatility"] / 100
-    
-    # Cap/floor IV for robustness
-    if sigma < 0.05:
+    # calculate volatility based on strike price + Cap/floor IV for robustness
+    sigma = gv.get_volatility(calls, K)
+    if sigma < 0.05:                                
         sigma = 0.2  # 20% for deep ITM
     if sigma > 1.0:
-        sigma = 0.5  # 50% for extreme OTM
+        sigma = 0.5  # 50% for extreme OTM                           
     
     
     # Find first expiry strictly in the future
-    now = dt.datetime.now()
-    first_expiry = None
-    for expiry in ticker.options:
-        expiry_dt = dt.datetime.strptime(expiry, "%Y-%m-%d").replace(hour=16)
-        if expiry_dt > now + dt.timedelta(hours=1):
-            first_expiry = expiry
-            break
-
-    if first_expiry is None:
-        raise ValueError("No valid future expirations")        
+    T = gv.find_expiry(ticker)
     
-    expiry_dt = dt.datetime.strptime(first_expiry, "%Y-%m-%d").replace(hour=16)
-    T = (expiry_dt - now).total_seconds() / (365.25 * 24 * 3600)
-    
-    print(f"S={S}, K={K}, T={T:.4f} yrs, r={r:.4f}, q={q:.4f}, sigma={sigma:.2f}")
 
     return [S, K, T, r, q, sigma]
 
